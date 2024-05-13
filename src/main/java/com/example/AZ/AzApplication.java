452408c37a0257a5ebff910d355f3c5ddb193e83
david <davidmalar88@gmail.com>
2024-05-13 18:43:56 +0200

    initial commit

---
+++ src/main/java/com/example/AZ/AzApplication.java
@@ -0,0 +1,13 @@
+package com.example.AZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AZ/AzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
